adfaa87038b6c3d4c5ec227866baa307efe1e921
Kimon Moutsakis <kmoutsakis@brighttalk.com>
2016-12-08 15:12:42 +0000

    added ga

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Software engineer with background in high traffic web applications">
 	<title>Kimon Moutsakis - Software Engineer</title>
+	<script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+        ga('create', 'UA-42770279-2', 'auto');
+        ga('send', 'pageview');
+
+	</script>
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/main.css') }}" rel="stylesheet">
 	<!-- Latest compiled and minified CSS -->
